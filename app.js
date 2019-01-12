547722cefd53ffa40feb38da855ea1f4f6de6e62
Sophie Hughes <sophie.hughes07@gmail.com>
2019-01-12 19:35:59 +0000

    updated so that the EU page is check as well as the US

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ var headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
-var options = {
+var optionsA = {
     url: 'http://www.yugioh-card.com/en/limited',
+    method: 'GET',
+    headers: headers
+}
+
+var optionsB = {
+    url: 'http://www.yugioh-card.com/oc/limited',
     method: 'GET',
     headers: headers
 }
@@ -40,13 +46,14 @@ var banlistHtml = '';
 
 function konamiRequest() {
 
-    request(options, function (error, response, body) {
+    request(optionsA, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             banlistHtml = response.body;
         }
     })
 
     var effectiveFrom = banlistHtml.match('Effective from ([A-Za-z]{2,10}) ([0-9]{2}), ([0-9]{4})');
+    console.log("Checking the banlist...");
 
     if (effectiveFrom != null) {
         effectiveDate = effectiveFrom[0].match('([A-Za-z]{2,10}) ([0-9]{2}), ([0-9]{4})');
@@ -59,6 +66,33 @@ function konamiRequest() {
             })
             console.log("I should have tweeted that the banlist updated");
             lastDate = d1;
+            return;
+        }
+    }
+
+    request(optionsB, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            banlistHtml = response.body;
+        }
+    })
+
+    var effectiveFrom = banlistHtml.match('Effective from ^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$');
+    console.log("Checking the banlist...");
+
+    if (effectiveFrom != null) {
+        effectiveDate = effectiveFrom[0].match('^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$');
+        var splitDate = effectiveDate.split('/');
+        var month = splitDate[1] - 1; //Javascript months are 0-11
+
+        var d2 = new Date(splitDate[2], month, splitDate[0]);
+
+        if (d2 >= Date.now() && d2 != lastDate) {
+            var tweet = 'The YuGiOh banlist has been updated! Effective from ' + effectiveDate[0];
+            T.post('statuses/update', {status: tweet}, function (err, data, response) {
+                console.log(data)
+            })
+            console.log("I should have tweeted that the banlist updated");
+            lastDate = d2;
         }
     }
 }
